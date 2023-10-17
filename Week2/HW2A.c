c11239042b4fcbffd7bc3dc0b5a3bce2cf3c59a2
noob <79190524+yinxiehaung@users.noreply.github.com>
2023-10-17 19:16:47 +0800

    Update HW2A.c
    改進x,y的範圍判斷，以及達到求出真正意義上的八個方向可以下的位置

--- Week2/HW2A.c
+++ Week2/HW2A.c
@@ -16,40 +16,66 @@ int main(){
     }
 
     int color = chess[address_y][address_x];
+    
+    for( int i = 0; i < 8; i++){
+        for( int j = 0; j < 8; j++){
+            if( chess[i][j] == color ){
+                
+                for( int dy = -1; dy <= 1; dy++ ){
+                    for( int dx = -1; dx <=1; dx++ ){
+                        int con = 0;
+                        int x = j, y = i;
+                        for( int k = 0; k < 8; k++ ){
+                            x += dx;
+                            y += dy;
+                            
+                            if( (x >= 0 && x < 8) && (y >= 0 && y < 8) ){
+                                if( chess[y][x] == 0 && con ){
+                                    chess[y][x] = 120;
+                                    con = 0;
+                                    continue;
+                                }
+
+                                if( chess[y][x] != color && chess[y][x] != 0 && chess[y][x] != 120){
+                                    con = 1;
+                                }else{
+                                    con = 0;
+                                }
+                            }
+                        }
+                    } 
+                }
+            }
+        }
+    }
 
     for( int dy = -1; dy <= 1; dy++ ){
-        for( int dx = -1; dx <=1; dx++ ){
+        for( int dx = -1; dx <= 1; dx++ ){
             int x = address_x, y = address_y, con = 0;
-
             for( int i = 0; i < 8; i++ ){
                 x += dx;
                 y += dy;
-
-                if( x > 0 && x < 8 && y > 0 && y < 8 ){
-                    if( chess[y][x] == 0 ){
-                        break;
-                    }
-                    if( chess[y][x] != color){
-                        con = 1;
-                    }else{
-                        con = 0;
+                if( (x >= 0 && x < 8) && (y >= 0 && y < 8) ){
+                    if( chess[y][x] == 120 ){
+                        chess[y][x] = -1;
+                        continue;
                     }
                 }
             }
-
-            if( con ){
-                chess[y][x] = 120;
-            }
-        } 
+        }
     }
+    
 
     for( int i = 0; i < 8; i++ ){
         for( int j = 0; j < 8; j++ ){
-            if( chess[i][j] != 120){
-                printf("%d ", chess[i][j]);
+            if( chess[i][j] == -1){
+                printf("%c ", 'x');
+                continue;
+            }else if( chess[i][j] == 120){
+                printf("%d ", 0);
                 continue;
             }
-            printf("%c ", chess[i][j]);
+            printf("%d ", chess[i][j]);
         }
         printf("\n");
     }
