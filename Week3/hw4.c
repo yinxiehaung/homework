e473a5ae2c2b237e0193824c679463c9c523fc39
noob <79190524+yinxiehaung@users.noreply.github.com>
2023-10-22 13:44:09 +0800

    Update hw4.c

--- Week3/hw4.c
+++ Week3/hw4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int _chess[8][8] = {}, _color;
+const int _direction[8][2] = {{-1,-1}, {-1,0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}; 
 
 void reset(){
     for( int i = 0; i < 8; i++ ){
@@ -36,15 +37,13 @@ int oneDirection(int x, int y, int dy, int dx ){
 }
 
 void check( int x, int y ){
-    for( int dy = -1; dy <= 1; dy++ ){
-        for( int dx = -1; dx <=1; dx++ ){
-            int step = oneDirection( x, y, dy, dx );
+   for( int i = 0; i < 8; i++){
+        int step = oneDirection( x, y, _direction[i][0], _direction[i][1] );
             
-            if(step){
-                printf("(%d, %d) ", x + (dx * step), y + (dy * step));
-            }           
+        if(step){
+            printf("(%d, %d) ", x + (dx * step), y + (dy * step));
         }
-    }
+   }
     reset();
 }
 
